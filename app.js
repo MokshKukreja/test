085384c28de9770165caaaabd8ec4401563a1531
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2024-05-23 13:35:01 +0530

    del

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 8000
-
-
-// app.use((req,res,next)=>{
-//     console.log("HTTPS Method" + req.method + " , URL" +req.url)
-//     next();
-// })
-// app.use("/transactions",transactionRouter)
-// app.use("/users",userRouter)
-
-
-
-// mongoose.connect(process.env.MONGO_URL, () => {
-//     console.log("Connected successfully");
-// })
-// mongoose.set('strictQuery', true);
-// mongoose.connect("mongodb+srv://mokshkukreja999:moksh90@cluster0.frzozja.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", () => {
-//     console.log("Connected successfully");
-// })
-// mongoose.set('strictQuery', true);
-
+const port = 9000
 
 
 app.use('/', (req, res) => {
     res.json({messgae:"Hello"})
 })
 
-app.listen(8000, () => {
+app.listen(9000, () => {
     console.log(`Example app listening on port ${port}`)
 })
