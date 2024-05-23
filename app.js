8da4f789970579ede793a6b28d11f961bf734949
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2024-05-23 13:10:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const cors = require("cors")
+
+
+
+app.use(express.json())
+
+
+app.use(cors())
+
+// app.use((req,res,next)=>{
+//     console.log("HTTPS Method" + req.method + " , URL" +req.url)
+//     next();
+// })
+// app.use("/transactions",transactionRouter)
+// app.use("/users",userRouter)
+
+
+
+// mongoose.connect(process.env.MONGO_URL, () => {
+//     console.log("Connected successfully");
+// })
+// mongoose.set('strictQuery', true);
+// mongoose.connect("mongodb+srv://mokshkukreja999:moksh90@cluster0.frzozja.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", () => {
+//     console.log("Connected successfully");
+// })
+// mongoose.set('strictQuery', true);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(8000, () => {
+    console.log(`Example app listening on port ${port}`)
+})
