fb3211db15e750aae98e685cab8d6a88fc3feb0b
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2024-05-23 13:30:54 +0530

    12

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8000
-const cors = require("cors")
 
-
-
-app.use(express.json())
-
-
-app.use(cors())
 
 // app.use((req,res,next)=>{
 //     console.log("HTTPS Method" + req.method + " , URL" +req.url)
